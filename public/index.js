307e2ee18058f0fd255d75be3b2af884ba1709f3
Jiliana Tan <136918826+jilianatan@users.noreply.github.com>
2023-06-29 13:22:19 +0800

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,19 @@
+/* 
+    Complete the JavaScript file below.
+    You have full control over this JavaScript file.
+    You may use jQuery or stick with vanilla JavaScript. 
+*/
+
+const submitBtn = document.querySelector("#submitBtn");
+
+submitBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // TODO: Write the code for the `Task Creation` functionality
+
+
+});
+
+// TODO: Write the code for the `Toggle Mark as Done` functionality
+
+// TODO, BONUS: Write the code for the `Sort Task by ..` functionality
